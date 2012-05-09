ecef6d8e83459924bbbf5e5c5ada14b92bf8640a
karan.jude@gmail.com <karan.jude@gmail.com@1af162b8-0f4c-4254-4929-3de44b247cc4>
2012-05-09 04:43:32 +0000

    add header features scripts

--- naive_bayes/header_body_features/process_data.py
+++ naive_bayes/header_body_features/process_data.py
@@ -9,6 +9,10 @@ if len(sys.argv) > 3:
 else:
     trainer = ""
 
+if len(sys.argv) > 4:
+    fr = True
+else:
+    fr = False
 
 def build_classifierss(src_dir, trainer):
     for l in os.listdir(src_dir):
@@ -76,10 +80,10 @@ def filter_output(src_dir, trainer):
         print command
         os.system(command)
 
-
-#generate_mallet_files(src_dir)
-#build_classifierss(src_dir, trainer)
-#test_classifier(src_dir, trainer)
-#filter_output(src_dir,trainer)
-#generate_accuracy(src_dir, trainer)
+if fr == False:
+    generate_mallet_files(src_dir)
+    build_classifierss(src_dir, trainer)
+    test_classifier(src_dir, trainer)
+    filter_output(src_dir,trainer)
+    generate_accuracy(src_dir, trainer)
 format_accuracy(src_dir, trainer)
